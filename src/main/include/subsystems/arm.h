fa65f4dd263e35eb709ef9eac9e488cf208a6785
okcroboticsteam <okcroboticsteam@gmail.com>
2023-01-17 19:54:22 -0600

    PID controllers

---
+++ src/main/include/subsystems/arm.h
@@ -0,0 +1,20 @@
+#include <frc2/command/SubsystemBase.h>
+#include <frc/controller/PIDController.h>
+#include <IO/armIO.h>
+
+
+class Arm : public frc2::SubsystemBase {
+public:
+Arm(ArmSoftwareInterface *interface)
+        : interface_(interface){}
+    ~Arm() {}
+
+    bool Function(int number);
+    bool SetDegrees(double degrees);
+    bool SetExtend(double inches);
+    bool init(); 
+    void Periodic() override;
+private:
+    ArmSoftwareInterface *const interface_;
+    frc2::PIDController *Arm_pid;
+};
