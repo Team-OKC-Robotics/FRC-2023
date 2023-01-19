b6eb8d6187d80f8e3370b3e4d26e5b5cec96b9f5
okcroboticsteam <okcroboticsteam@gmail.com>
2023-01-19 17:28:06 -0600

    arm pids

--- src/main/cpp/subsystems/arm.cpp
+++ src/main/cpp/subsystems/arm.cpp
@@ -15,14 +15,14 @@ this->Arm_pid->SetSetpoint (degrees);
 }
 
 bool Arm::SetExtend(double inches){
+this->Inches_pid->SetSetpoint (inches);
+   
     return true;
 }
 
-bool Arm::Function(int number){
-    return true;
-}
 
-void Arm::Periodic(){
+    void Arm::Periodic(){
     this->interface_->arm_lift_power = this->Arm_pid->Calculate(this->interface_->arm_encoder);
+    this->interface_->arm_extend_power = this->Inches_pid->Calculate(this->interface_->arm_extend_encoder);
 
 }
---
+++ src/main/include/subsystems/arm.h
@@ -0,0 +1,22 @@
+#include <frc2/command/SubsystemBase.h>
+#include <frc/controller/PIDController.h>
+#include <IO/armIO.h>
+
+
+class Arm : public frc2::SubsystemBase {
+public:
+Arm(ArmSoftwareInterface *interface)
+        : interface_(interface){}
+    ~Arm() {}
+
+    bool Function(int number);
+    bool SetDegrees(double degrees);
+    bool SetExtend(double inches);
+    bool init(); 
+    void Periodic() override;
+private:
+    ArmSoftwareInterface *const interface_;
+    std::shared_ptr<frc::PIDController> Arm_pid;
+    std::shared_ptr<frc::PIDController> Inches_pid;
+    std::shared_ptr<frc::PIDController> Int_pid;
+};
