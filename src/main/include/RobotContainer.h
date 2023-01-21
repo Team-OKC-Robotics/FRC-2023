d7e158ad4233106fc6955f0e0ffe7be893ab57f9
AbbeySieg <119759599+AbbeySieg@users.noreply.github.com>
2023-01-21 13:33:59 -0600

    Feat/hardware (#9)
    * added hardware files
    
    * changed names of motors
    
    * defined
    
    * labeled buttons
    
    * arm sw, hw, cpp, and IO
    
    * changed file names
    
    * renamed more files
    
    * added swerve drive to branch
    
    * changed hw, actua, and sen names
    
    * it's being weird
    
    * changed names
    
    * fix errors
    
    * fixed errors
    
    * fixed errors
    
    * fixed errors
    
    * made changes Justin wanted
    
    * fixed errors
    
    * fix NavX library and Sensors.h
    
    Co-authored-by: okcroboticsteam <okcroboticsteam@gmail.com>
    Co-authored-by: danielbrownmsm <daniel.brown@msmrockets.org>
    Co-authored-by: Justin Kleiber <jkleiber8@gmail.com>

--- src/main/include/RobotContainer.h
+++ src/main/include/RobotContainer.h
@@ -8,6 +8,9 @@
 
 
 
+#include <frc/Joystick.h>
+#include <frc2/command/button/JoystickButton.h>
+
 /**
  * This class is where the bulk of the robot should be declared.  Since
  * Command-based is a "declarative" paradigm, very little robot logic should
@@ -26,4 +29,44 @@ class RobotContainer {
  
 
   void ConfigureButtonBindings();
+
+    std::shared_ptr<frc::Joystick> gamepad1_;
+    std::shared_ptr<frc::Joystick> gamepad2_;
+
+    std::shared_ptr<frc2::JoystickButton> driver_A_button_;
+    std::shared_ptr<frc2::JoystickButton> driver_B_button_;
+    std::shared_ptr<frc2::JoystickButton> driver_X_button_;
+    std::shared_ptr<frc2::JoystickButton> driver_Y_button_;
+    std::shared_ptr<frc2::JoystickButton> driver_RB_button_;
+    std::shared_ptr<frc2::JoystickButton> driver_LB_button_;
+    std::shared_ptr<frc2::JoystickButton> driver_RT_button_;
+    std::shared_ptr<frc2::JoystickButton> driver_LT_button_;
+    std::shared_ptr<frc2::JoystickButton> driver_back_button_;
+    std::shared_ptr<frc2::JoystickButton> driver_start_button_;
+    std::shared_ptr<frc2::JoystickButton> driver_mode_button_;
+
+    std::shared_ptr<frc2::JoystickButton> manip_a_button_;
+    std::shared_ptr<frc2::JoystickButton> manip_b_button_;
+    std::shared_ptr<frc2::JoystickButton> manip_back_button_;
+    std::shared_ptr<frc2::JoystickButton> manip_start_button_;
+    std::shared_ptr<frc2::JoystickButton> manip_left_stick_button_;
+    std::shared_ptr<frc2::JoystickButton> manip_right_stick_button;
+
+    std::shared_ptr<frc2::JoystickButton> manip_trigger_button_;
+    std::shared_ptr<frc2::JoystickButton> manip_left_button_;
+    std::shared_ptr<frc2::JoystickButton> manip_bottom_right_button_;
+    std::shared_ptr<frc2::JoystickButton> manip_top_right_button_;
+    std::shared_ptr<frc2::JoystickButton> manip_botton_left_button_;
+    std::shared_ptr<frc2::JoystickButton> manip_top_left_button_;
+
+    std::shared_ptr<frc2::JoystickButton> manip_seven_button_;
+    std::shared_ptr<frc2::JoystickButton> manip_eight_button_;
+    std::shared_ptr<frc2::JoystickButton> manip_nine_button_;
+    std::shared_ptr<frc2::JoystickButton> manip_ten_button_;
+    std::shared_ptr<frc2::JoystickButton> manip_eleven_button_;
+    std::shared_ptr<frc2::JoystickButton> manip_twelve_button_;
+
+
 };
+
+
---
+++ src/main/include/hardware/Actuators.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <memory>
+#include <rev/CANSparkMax.h>
+
+
+
+
+// Brushless Motor
+#define BRUSHLESS rev::CANSparkMax::MotorType::kBrushless
+
+// Coast
+#define COAST rev::CANSparkMax::IdleMode::kCoast
+
+
+
+typedef struct actuators_t {
+    // Left drivetrain motors
+    std::unique_ptr<rev::CANSparkMax> left_front_drive;
+    std::unique_ptr<rev::CANSparkMax> left_back_drive;
+    std::unique_ptr<rev::CANSparkMax> left_front_steer;
+    std::unique_ptr<rev::CANSparkMax> left_back_steer;
+
+    // Right drivetrain motors
+    std::unique_ptr<rev::CANSparkMax> right_front_drive;
+    std::unique_ptr<rev::CANSparkMax> right_back_drive;
+    std::unique_ptr<rev::CANSparkMax> right_front_steer;
+    std::unique_ptr<rev::CANSparkMax> right_back_steer;
+
+    // arm motors
+    std::unique_ptr<rev::CANSparkMax> arm_lift_motor;
+    std::unique_ptr<rev::CANSparkMax> arm_up_motor;
+    std::unique_ptr<rev::CANSparkMax> arm_extend_motor;
+} Actuators;
