dbcbc023a01aadfab2bfadd95125885774cc01bc
Justin Kleiber <jkleiber8@gmail.com>
2023-01-25 22:00:53 -0500

    #28: Updates clang-format file

--- src/main/include/Utils.h
+++ src/main/include/Utils.h
@@ -4,49 +4,51 @@
 #include <iostream>
 
 #define OKC_CALL(res)                                                          \
-  if (!(res)) {                                                                \
-    std::cerr << "[" << __FILE__ << "(" << __LINE__ << ")] - OKC_CALL("        \
-              << #res << ")" << std::endl;                                     \
-    return false;                                                              \
-  }
+    if (!(res)) {                                                              \
+        std::cerr << "[" << __FILE__ << "(" << __LINE__ << ")] - OKC_CALL("    \
+                  << #res << ")" << std::endl;                                 \
+        return false;                                                          \
+    }
 
 #define OKC_CHECK(check)                                                       \
-  if (!(check)) {                                                              \
-    std::cerr << "[" << __FILE__ << "(" << __LINE__ << ")] - OKC_CHECK("       \
-              << #check << ")" << std::endl;                                   \
-    return false;                                                              \
-  }
+    if (!(check)) {                                                            \
+        std::cerr << "[" << __FILE__ << "(" << __LINE__ << ")] - OKC_CHECK("   \
+                  << #check << ")" << std::endl;                               \
+        return false;                                                          \
+    }
 
 #define OKC_CHECK_MSG(check, msg)                                              \
-  if (!(check)) {                                                              \
-    std::cerr << "[" << __FILE__ << "(" << __LINE__ << ")] - OKC_CHECK_MSG("   \
-              << #check << "): " << msg << std::endl;                          \
-    return false;                                                              \
-  }
+    if (!(check)) {                                                            \
+        std::cerr << "[" << __FILE__ << "(" << __LINE__                        \
+                  << ")] - OKC_CHECK_MSG(" << #check << "): " << msg           \
+                  << std::endl;                                                \
+        return false;                                                          \
+    }
 
 #define VOKC_CALL(res)                                                         \
-  if (!(res)) {                                                                \
-    std::cerr << "[" << __FILE__ << "(" << __LINE__ << ")] - VOKC_CALL("       \
-              << #res << ")" << std::endl;                                     \
-    return;                                                                    \
-  }
+    if (!(res)) {                                                              \
+        std::cerr << "[" << __FILE__ << "(" << __LINE__ << ")] - VOKC_CALL("   \
+                  << #res << ")" << std::endl;                                 \
+        return;                                                                \
+    }
 
 #define VOKC_CHECK(check)                                                      \
-  if (!(check)) {                                                              \
-    std::cerr << "[" << __FILE__ << "(" << __LINE__ << ")] - VOKC_CHECK("      \
-              << #check << ")" << std::endl;                                   \
-    return;                                                                    \
-  }
+    if (!(check)) {                                                            \
+        std::cerr << "[" << __FILE__ << "(" << __LINE__ << ")] - VOKC_CHECK("  \
+                  << #check << ")" << std::endl;                               \
+        return;                                                                \
+    }
 
 #define VOKC_CHECK_MSG(check, msg)                                             \
-  if (!(check)) {                                                              \
-    std::cerr << "[" << __FILE__ << "(" << __LINE__ << ")] - VOKC_CHECK_MSG("  \
-              << #check << "): " << msg << std::endl;                          \
-    return;                                                                    \
-  }
+    if (!(check)) {                                                            \
+        std::cerr << "[" << __FILE__ << "(" << __LINE__                        \
+                  << ")] - VOKC_CHECK_MSG(" << #check << "): " << msg          \
+                  << std::endl;                                                \
+        return;                                                                \
+    }
 
 namespace TeamOKC {
 
-bool Clamp(const double &lower, const double &upper, double *value);
+    bool Clamp(const double &lower, const double &upper, double *value);
 
 }
--- src/test/cpp/utils_test.cpp
+++ src/test/cpp/utils_test.cpp
@@ -6,30 +6,30 @@
 
 class UtilsTest : public testing::Test {
 public:
-  virtual void SetUp() {
-    // Nothing to set up
-  }
+    virtual void SetUp() {
+        // Nothing to set up
+    }
 
 protected:
-  bool OkcChecker(bool check) {
-    OKC_CHECK(check);
+    bool OkcChecker(bool check) {
+        OKC_CHECK(check);
 
-    return true;
-  }
+        return true;
+    }
 
-  bool OkcCallChecker(bool return_val) {
-    OKC_CALL(OkcChecker(return_val));
-    return true;
-  }
+    bool OkcCallChecker(bool return_val) {
+        OKC_CALL(OkcChecker(return_val));
+        return true;
+    }
 };
 
 TEST_F(UtilsTest, OkcCheckTest) {
-  std::shared_ptr<bool> b = nullptr;
-  ASSERT_FALSE(OkcChecker(b != nullptr));
-  ASSERT_TRUE(OkcChecker(b == nullptr));
+    std::shared_ptr<bool> b = nullptr;
+    ASSERT_FALSE(OkcChecker(b != nullptr));
+    ASSERT_TRUE(OkcChecker(b == nullptr));
 }
 
 TEST_F(UtilsTest, OkcCallTest) {
-  ASSERT_FALSE(OkcCallChecker(false));
-  ASSERT_TRUE(OkcCallChecker(true));
+    ASSERT_FALSE(OkcCallChecker(false));
+    ASSERT_TRUE(OkcCallChecker(true));
 }
