724099abe822739dd95b61be72e744789601a2b9
deadlyvortex <deadlyvortex944@hotmail.com>
2023-02-13 18:32:46 -0600

    FIxed Indentation

--- src/main/include/Subsystems/Vision.h
+++ src/main/include/Subsystems/Vision.h
@@ -5,22 +5,22 @@
 #include "io/VisionIO.h"
 
 class Vision : public frc2::Subsystem {
-    public:
-    Vision(VisionSoftwareInterface *interface)
-        : interface_(interface) {}
+public:
+Vision(VisionSoftwareInterface *interface)
+: interface_(interface) {}
 
 
-        bool Init();
-        void Periodic() override;
-        void SimulationPeriodic() override;
+bool Init();
+void Periodic() override;
+void SimulationPeriodic() override;
 
-        bool GetConeError (double *error);
-        bool GetConeDistance (double *cone);
-        bool GetCubeDistance (double *cube);
-        bool GetCubeAngle (double *angle);
-        bool ResetSubsystem ();
+bool GetConeError (double *error);
+bool GetConeDistance (double *cone);
+bool GetCubeDistance (double *cube);
+bool GetCubeAngle (double *angle);
+bool ResetSubsystem ();
 
-        private:
-        VisionSoftwareInterface *const interface_;
+private:
+VisionSoftwareInterface *const interface_;
 
 };
