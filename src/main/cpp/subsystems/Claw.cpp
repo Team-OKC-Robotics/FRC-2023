229634c7061e78e91274787f3b52be017aa73f6f
danielbrownmsm <daniel.brown@msmrockets.org>
2023-02-24 21:49:17 -0600

    commented Claw subsystem

--- src/main/cpp/subsystems/Claw.cpp
+++ src/main/cpp/subsystems/Claw.cpp
@@ -4,17 +4,21 @@
 #include "ui/UserInterface.h"
 
 bool Claw::Init() {
+    // read PID values from the parameters file
     double kP = RobotParams::GetParam("claw.claw_pid.kP", 0.01);
     double kI = RobotParams::GetParam("claw.claw_pid.kI", 0.0);
     double kD = RobotParams::GetParam("claw.claw_pid.kD", 0.0);
 
+    // create the claw PID
     claw_pid_ = std::make_shared<frc::PIDController>(kP, kI, kD);
     claw_power_ = 0;
 
+    // logs for the claw values
     claw_output_log_ = wpi::log::DoubleLogEntry(TeamOKC::log, "/claw/claw_output");
     claw_enc_log_ = wpi::log::DoubleLogEntry(TeamOKC::log, "/claw/claw_enc");
     claw_setpoint_log_ = wpi::log::DoubleLogEntry(TeamOKC::log, "/claw/claw_setpoint");
 
+    // preset positions from parameters.toml
     open_pos_ = RobotParams::GetParam("claw.open", 0);
     cube_pos_ = RobotParams::GetParam("claw.cube", 0);
     cone_pos_ = RobotParams::GetParam("claw.cone", 0);
@@ -37,6 +41,8 @@ bool Claw::ResetPositionPID() {
 }
 
 bool Claw::SetPreset(ClawPreset preset) {
+    // set the claw to a particular preset
+
     if (preset == OPEN) {
         this->SetPosition(open_pos_);
     } else if (preset == CUBE) {
@@ -44,6 +50,7 @@ bool Claw::SetPreset(ClawPreset preset) {
     } else if (preset == CONE) {
         this->SetPosition(cone_pos_);
     } else {
+        // we shouldn't have gotten here, so let someone know something weird happened
         OKC_CHECK_MSG(false, "unhandled claw enum value");
     }
 
@@ -51,12 +58,14 @@ bool Claw::SetPreset(ClawPreset preset) {
 }
 
 bool Claw::SetPosition(double pos) {
+    // set the septoint of the claw PID controller
     claw_pid_->SetSetpoint(pos);
 
     return true;
 }
 
 bool Claw::Reset() {
+    // reset the subsystem
     this->ResetPositionEncoder();
     this->ResetPositionPID();
 
@@ -64,9 +73,10 @@ bool Claw::Reset() {
 }
 
 void Claw::Periodic() {
+    // PID to the claw's setpoint
     interface_->claw_open_and_close_power = this->claw_pid_->Calculate(interface_->encoder_reading);
 
-    // logs
+    // log the values
     claw_output_log_.Append(interface_->claw_open_and_close_power);
     claw_enc_log_.Append(interface_->encoder_reading);
     claw_setpoint_log_.Append(claw_pid_->GetSetpoint());
