9e2d332aa611d9f89adae71289c8890dca71dcca
danielbrownmsm <daniel.brown@msmrockets.org>
2023-03-30 06:53:39 -0500

    start work on a python arm vizualization script

---
+++ arm_viz.py
@@ -0,0 +1,22 @@
+import pygame
+
+# create our hardware surface object
+screen = pygame.display.set_mode((800, 600))
+
+# set the title for the window
+pygame.display.set_caption("ArmViz v 0.1")
+
+# go ahead and clear window
+screen.fill(WHITE)
+
+# make ourselves a font for displaying text
+font = pygame.font.Font(None, 48)
+
+# make ourselves a clock as well
+clock = pygame.time.Clock()
+
+# main loop
+while True:
+    #TODO
+    
+    pass
