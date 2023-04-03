67e5ba6f92c90f30c8ea473e67d76a9b755d9352
danielbrownmsm <daniel.brown@msmrockets.org>
2023-04-03 16:29:43 -0500

    draw the joystick values as well in the arm_viz script

--- arm_viz.py
+++ arm_viz.py
@@ -1,10 +1,13 @@
-import pygame
+import pygame, time
 from math import sin, cos, radians
 import process_wpilib_logs
 
 BASE_ARM_LENGTH = 200
 WIDTH = 800
 HEIGHT = 800
+HEIGHT_OFFSET = 0
+WIDTH_OFFSET = 150
+WHEEL_LENGTH = 100
 
 # state variables
 arm_length_ = 0
@@ -18,6 +21,8 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
 LIGHT_BLUE = (100, 100, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 
 # create our hardware surface object
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -38,12 +43,18 @@ print(len(process_wpilib_logs.liftEncLog[1]))
 
 index = 0
 frame = 0
+advance = False
 
 # search for where the setpoint changes (where things actually start to happen)
 for idx, setpoint in enumerate(process_wpilib_logs.liftSetpointLog[1]):
     if setpoint != 0:
         index = idx
         break
+
+# for idx, val in enumerate(process_wpilib_logs.joystickTurnLog[1]):
+#     if val != 0:
+#         index = idx
+#         break
 
 # main loop
 while True:
@@ -53,28 +64,44 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             raise SystemExit
-
-    #TODO
+        elif event.type == pygame.KEYDOWN:
+            pass
 
     length_setpoint_ = BASE_ARM_LENGTH + process_wpilib_logs.extendSetpointLog[1][index]
     arm_length_ = BASE_ARM_LENGTH + process_wpilib_logs.extendEncLog[1][index]
 
     # draw the setpoint
-    setpoint_x = sin(radians(process_wpilib_logs.liftSetpointLog[1][index])) * length_setpoint_  +  WIDTH//2
-    setpoint_y = cos(radians(process_wpilib_logs.liftSetpointLog[1][index])) * length_setpoint_  +  HEIGHT//2
-    pygame.draw.line(screen, LIGHT_BLUE, (WIDTH//2, HEIGHT//2), (setpoint_x, setpoint_y), 20)
+    setpoint_x = sin(radians(process_wpilib_logs.liftSetpointLog[1][index])) * length_setpoint_  +  WIDTH//2 + WIDTH_OFFSET
+    setpoint_y = cos(radians(process_wpilib_logs.liftSetpointLog[1][index])) * length_setpoint_  +  HEIGHT//2 + HEIGHT_OFFSET
+    pygame.draw.line(screen, LIGHT_BLUE, (WIDTH//2 + WIDTH_OFFSET, HEIGHT//2 + HEIGHT_OFFSET), (setpoint_x, setpoint_y), 20)
 
     # draw the current arm tilt from the middle of the window with a length of BASE_ARM_LENGTH, and a width of 10
-    x = sin(radians(process_wpilib_logs.liftEncLog[1][index])) * arm_length_  +  WIDTH//2
-    y = cos(radians(process_wpilib_logs.liftEncLog[1][index])) * arm_length_  +  HEIGHT//2
-    pygame.draw.line(screen, BLUE, (WIDTH//2, HEIGHT//2), (x, y), 15)
-    
-    # advance the time once every 2 frames
-    frame += 1
-    if frame % 1 == 0:
-        index += 1
-    pygame.display.flip()
-    clock.tick(60)
+    x = sin(radians(process_wpilib_logs.liftEncLog[1][index])) * arm_length_  +  WIDTH//2 + WIDTH_OFFSET
+    y = cos(radians(process_wpilib_logs.liftEncLog[1][index])) * arm_length_  +  HEIGHT//2 + HEIGHT_OFFSET
+    pygame.draw.line(screen, BLUE, (WIDTH//2 + WIDTH_OFFSET, HEIGHT//2 - HEIGHT_OFFSET), (x, y), 15)
 
-    # if process_wpilib_logs.liftEncLog[0][index] > 200:
-        # raise SystemExit
+    # draw the non-arm swerve drive stuff (for the heck of it, otherwise watching this is too boring)
+    #   |\
+    # y | \
+    #   |  \
+    #   |   \
+    #   ====== x
+    steer_x = sin(radians(process_wpilib_logs.steerEncLog[1][index])) * WHEEL_LENGTH +  WIDTH//2 - WIDTH_OFFSET
+    steer_y = cos(radians(process_wpilib_logs.steerEncLog[1][index])) * WHEEL_LENGTH +  HEIGHT//2 - HEIGHT_OFFSET
+    pygame.draw.line(screen, GREEN, (WIDTH//2 - WIDTH_OFFSET, HEIGHT//2 - HEIGHT_OFFSET), (steer_x, steer_y), 15)
+    pygame.draw.line(screen, GREEN, (steer_x, steer_y), (WIDTH//2 - WIDTH_OFFSET, HEIGHT//2 - HEIGHT_OFFSET), 15)
+    
+    # draw the joystick values as well
+    # turn is simply a horizontal line back and forth
+    pygame.draw.line(screen, GREEN, (WIDTH//2 + WIDTH_OFFSET, HEIGHT//4 - HEIGHT_OFFSET*2), (WIDTH//2 + WIDTH_OFFSET + process_wpilib_logs.joystickTurnLog[1][index] * 25, HEIGHT//4 - HEIGHT_OFFSET*2), 10)
+
+    # and then we'll make the drive+strafe a vector. actually, I might not need to do anything fancy here
+    pygame.draw.line(screen, RED, (WIDTH//2 - WIDTH_OFFSET, HEIGHT//4 - HEIGHT_OFFSET*2), (WIDTH//2 - WIDTH_OFFSET + process_wpilib_logs.joystickStrafeLog[1][index] * 100, HEIGHT//4 - HEIGHT_OFFSET*2 + process_wpilib_logs.joystickDriveLog[1][index] * 40), 15)
+
+    pygame.display.flip()
+
+    # calculate time difference
+    if index > 1:
+        delta_t = process_wpilib_logs.driveOutputLog[0][index] - process_wpilib_logs.driveOutputLog[0][index - 1]
+        time.sleep(delta_t)
+    index += 1
