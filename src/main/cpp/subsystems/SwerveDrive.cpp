6b8da6a1b574f8fceb29171b14f54b440ccf30a3
deadlyvortex <deadlyvortex944@hotmail.com>
2023-10-16 17:26:06 -0500

    idk

--- src/main/cpp/subsystems/SwerveDrive.cpp
+++ src/main/cpp/subsystems/SwerveDrive.cpp
@@ -318,14 +318,14 @@ bool SwerveDrive::VectorTeleOpDrive(const double &drive, const double &strafe, c
         this->interface_->left_front_drive_motor_output = turn_ * 2;
         this->interface_->left_back_drive_motor_output= turn_ * 2;
         this->interface_->right_front_drive_motor_output = turn_ * 2;
-        this->interface_->right_back_drive_motor_output = turn_ * 2;
+        this->interface_->right_back_drive_motor_output = turn_ * 2; 
 
     }
 
-    OKC_CALL(this->left_front_module_->GetSteerOutput(&this->interface_->left_front_steer_motor_output));
+    OKC_CALL(this->left_front_module_->GetSteerOutput(&this->interface_->left_front_steer_motor_output)); 
     OKC_CALL(this->left_back_module_->GetSteerOutput(&this->interface_->left_back_steer_motor_output));
     OKC_CALL(this->right_front_module_->GetSteerOutput(&this->interface_->right_front_steer_motor_output));
-    OKC_CALL(this->right_back_module_->GetSteerOutput(&this->interface_->right_back_steer_motor_output));
+    OKC_CALL(this->right_back_module_->GetSteerOutput(&this->interface_->right_back_steer_motor_output)); 
 
     OKC_CHECK(this->interface_ != nullptr);
 
